067418b8dc56d6fc84ed5baded52143578ee3365
Cody Brunty <13463782+codybrunty@users.noreply.github.com>
2025-06-09 11:55:06 -0700

    simplified for now

--- PrototypeJC/Assets/Scripts/Factory/EnvironmentFactory.cs
+++ PrototypeJC/Assets/Scripts/Factory/EnvironmentFactory.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class EnvironmentFactory : IFactory<EnvironmentData, GameObject> {
     public GameObject Create(EnvironmentData envData) {
-        if (envData == null || envData.environmentPrefab == null) {
-            Debug.LogError("[EnvironmentFactory] Missing environment prefab!");
-            return null;
-        }
-
         return Object.Instantiate(envData.environmentPrefab);
     }
 }
